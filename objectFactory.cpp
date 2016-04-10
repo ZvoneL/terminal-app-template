f9ee9d2c2f13dc355b901aac58eb8dfaa371f115
Zvonimir Lucic <zvone.lucic@gmail.com>
2016-04-10 12:43:08 +0200

    adding objectFactory.cpp file

---
+++ objectFactory.cpp
@@ -0,0 +1,16 @@
+#include "objectFactory.h"
+
+ObjectFactory ObjectFactory::inst;
+
+ObjectFactory::ObjectFactory()
+{
+}
+
+ObjectFactory::~ObjectFactory()
+{
+}
+
+int ObjectFactory::Init()
+{
+	return 0;
+}
